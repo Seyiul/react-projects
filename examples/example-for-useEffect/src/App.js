30c3ed6ec66c749afcf72aa66b6ff5e1c85f96db
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-02-05 15:50:31 +0900

    example-for-useEffect

---
+++ examples/example-for-useEffect/src/App.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when 'counter & keyword' changes.");
+  }, [counter, keyword]);
+
+  return (
+    <div>
+      <input
+        value={keyword}
+        type="text"
+        placeholder="Search here..."
+        onChange={onChange}
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
